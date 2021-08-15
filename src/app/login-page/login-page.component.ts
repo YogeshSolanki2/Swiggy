8707478ffdb682d1c77b4cb8661450764e161e44
YogeshSolanki2 <yogesh.solanki2@outlook.com>
2021-08-15 20:13:05 +0530

    Validation on login Passowrd

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent  {
+  username='';
+  password='';
+ 
+  maxLength=20;
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern("^(?=.*[0-9])"+ "(?=.*[a-z])(?=.*[A-Z])"+ "(?=.*[@#$%^&+=])"+ "(?=\\S+$).{8,20}$")
+  ]);
+
   // constructor() { }
 
   // ngOnInit(): void {
   // }
+  // control = new FormControl('', Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$'));
+
+  //Login Button Function
+  logIn(){
+
+  }
+  //SignUp Button Function 
+  signUp(){
+
+  }
+
+
 
 }
+const control = new FormControl('YOgesh17#', Validators.pattern("^(?=.*[0-9])"+ "(?=.*[a-z])(?=.*[A-Z])"+ "(?=.*[@#$%^&+=])"+ "(?=\\S+$).{8,20}$"));
+
+console.log(control.errors); 
+
+
